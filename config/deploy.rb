e98cfe2a34549be3d3fc0ca1e8efdea0a340ed9f
Denis Kuznetsov <denis@f7.ru>
2013-04-07 15:04:24 +0400

    deploy fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
 require "bundler/capistrano"
 require 'sidekiq/capistrano'
 
+load 'deploy/assets'
+
 set :application, "vkblacklist"
 set :repository,  "git@github.com:byteg/vkblacklist.git"
 
 set :branch, fetch(:branch, "master")
 
-set :user, 'deploy'
 set :deploy_to, "/home/deploy/#{application}"
+set :user, 'deploy'
+set :use_sudo, false
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -15,6 +18,9 @@ set :deploy_to, "/home/deploy/#{application}"
 role :web, "vkblacklist.ru"                          # Your HTTP server, Apache/etc
 role :app, "vkblacklist.ru"                          # This may be the same as your `Web` server
 role :db,  "vkblacklist.ru", :primary => true # This is where Rails migrations will run
+
+before "deploy:finalize_update", "deploy:symlink_db_config"
+after "deploy", "deploy:migrate"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -24,7 +30,7 @@ role :db,  "vkblacklist.ru", :primary => true # This is where Rails migrations w
 
 # If you are using Passenger mod_rails uncomment this:
  namespace :deploy do
-   task :after_finalize_update do
+   task :symlink_db_config do
      run "ln -sf #{shared_path}/config/* #{latest_release}/config/"
      run "ln -sf #{shared_path}/config/robots.txt #{latest_release}/public/robots.txt"
    end
