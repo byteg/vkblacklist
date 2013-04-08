1452d7e137ddac2109cae4a82594acd07c58268e
Denis Kuznetsov <denis@f7.ru>
2013-04-08 15:07:05 +0400

    methods for ban & unban from admin

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -8,7 +8,7 @@ ActiveAdmin.register Group do
     column :banned
     column :checked
     column :ban_until
-    default_actions                   
+    default_actions
   end                                 
 
   filter :id
@@ -17,4 +17,18 @@ ActiveAdmin.register Group do
   filter :item_id
   filter :checked
   filter :ban_until
+
+
+  member_action :unban, :method => :put do
+    group = Group.find(params[:id])
+    group.unban!
+    redirect_to {:action => :show}, {:notice => "Unbanned!"}
+  end
+
+  member_action :ban, :method => :put do
+    group = Group.find(params[:id])
+    group.ban!
+    redirect_to {:action => :show}, {:notice => "Banned!"}
+  end
+
 end                                   
