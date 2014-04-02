b8fd77d0b75656f58b2dd8fe7d09602ce2f2f734
Denis Kuznetsov <denis@f7.ru>
2014-04-02 18:46:11 +0400

    fix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,7 +22,7 @@ class GroupsController < ApplicationController
     @group = Group.find_by_url(params[:url])
     
     respond_to do |format|
-      format.html { redirect_to root_path, notice: @group.nil? || !@group.banned? ? I18n.t('groups.not_in_list') : I18n.t('groups.in_list', :ban_until => l(@group.ban_until.in_time_zone('Moscow'), :format => :long) }
+      format.html { redirect_to root_path, notice: @group.nil? || !@group.banned? ? I18n.t('groups.not_in_list') : I18n.t('groups.in_list', :ban_until => l(@group.ban_until.in_time_zone('Moscow'), :format => :long)) }
       format.json { render json: { :result => @group.nil? || !@group.banned? ? 0 : 1 } }
     end
   end
