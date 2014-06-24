c90e5392a21d67e0995566914e0671163542eca5
Denis Kuznetsov <denis@f7.ru>
2014-06-24 11:06:50 +0400

    minimum 1 thread

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ daemonize true
 pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
 state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
 stdout_redirect "#{application_path}/log/puma-#{railsenv}.stdout.log", "#{application_path}/log/puma-#{railsenv}.stderr.log"
-threads 0, 16
+threads 1, 16
 bind "unix://#{application_path}/tmp/sockets/#{railsenv}.socket"
 
 workers 1
