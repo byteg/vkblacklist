59fa18369db7038a992adc6f060d5155a08307db
Denis Kuznetsov <denis@f7.ru>
2015-03-10 12:59:56 +0300

    sidekiq added namespace

--- config/clock.rb
+++ config/clock.rb
@@ -3,6 +3,15 @@
 require 'clockwork'
 require 'sidekiq'
 
+Sidekiq.configure_server do |config|
+  config.redis = { :url => 'redis://localhost:6379', :namespace => 'vkblacklist' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :url => 'redis://localhost:6379', :namespace => 'vkblacklist' }
+end
+
+
 class GroupsChecker
   include Sidekiq::Worker
 end
