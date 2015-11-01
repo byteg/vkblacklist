0582f9372a53946f337499fc20bc529df53e5e9d
Denis Kuznetsov <denis@f7.ru>
2015-11-01 18:41:02 +0300

    ban time counter updated

--- app/services/ban_time_counter.rb
+++ app/services/ban_time_counter.rb
@@ -6,18 +6,20 @@ class BanTimeCounter
   end
 
   def time_for_volk
-    # last_day_complaints_count = @group.complaints.for_volk.where('created_at > ?', 1.day.ago).count
-    # if @group.banned?
-    #   return 72.hours if last_day_complaints_count > 30
-    #   return 48.hours if last_day_complaints_count > 15
-    #   return 24.hours if last_day_complaints_count > 5
-    #   return 12.hours if last_day_complaints_count > 3
-    #   return 6.hours if last_day_complaints_count > 1
-    #   return VOLK_DEFAULT_VALUE
-    # else
-    #   return last_day_complaints_count > 0 ? 24.hours : VOLK_DEFAULT_VALUE
-    # end
-    VOLK_DEFAULT_VALUE
+    last_day_complaints_count = @group.complaints.for_volk.where('created_at > ?', 1.day.ago).count
+    if @group.banned?
+      return 72.hours if last_day_complaints_count > 300
+      return 50.hours if last_day_complaints_count > 100
+      return 30.hours if last_day_complaints_count > 30
+      return 24.hours if last_day_complaints_count > 15
+      return 12.hours if last_day_complaints_count > 5
+      return 6.hours if last_day_complaints_count > 3
+      return 3.hours if last_day_complaints_count > 1
+      return VOLK_DEFAULT_VALUE
+    else
+      return last_day_complaints_count > 0 ? 24.hours : VOLK_DEFAULT_VALUE
+    end
+    # VOLK_DEFAULT_VALUE
   end
   
 end
