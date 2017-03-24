1a2f0f83827794a949c2feef4bc46222c7f81be1
Denis Kuznetsov <denis@f7.ru>
2017-03-24 12:47:22 +0300

    proxy list changed

--- app/services/proxy_list.rb
+++ app/services/proxy_list.rb
@@ -3,8 +3,8 @@ class ProxyList
                 {host: '82.146.58.164', port: 8888}, #firstvds, byte
                 {host: '188.120.244.132', port: 8888}, #firstvds, byte
                 {host: '188.120.242.79', port: 8888}, #firstvds, byte
-                {host: '188.120.237.86', port: 8888}, #firstvds, byte
-                {host: '188.120.236.216', port: 8888}, #firstvds, bytecheg@gmail.com
+                {host: '188.120.237.86', port: 8888} #firstvds, byte
+                #{host: '188.120.236.216', port: 8888}, #firstvds, bytecheg@gmail.com
                 nil
               ]
 
